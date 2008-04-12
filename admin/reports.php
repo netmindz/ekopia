cd5db8fc5582ab6027e7748f5510255417587489
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-04-12 12:35:07 +0000

    feature: add new report for screwed tracks

--- admin/reports.php
+++ admin/reports.php
@@ -4,7 +4,9 @@ require("/home/www/codebase/database.php");
 require("/home/www/codebase/fpdf.php");
 $reports = array();
 
-$reports['album_list'] =  array("title"=>"Album List","sql"=>"select name,price,stock_count from albums order by name");
+$reports['album_list'] =  array("title"=>"Album List","sql"=>"select id,name,price,stock_count from albums order by name");
+$reports['unamed'] =  array("title"=>"Unnamed Tracks","sql"=>"select albums.id,albums.name,sum(if(tracks.name = '',1,0)) as unnamed from tracks left join albums on album_id=albums.id group by albums.id having unamed > 0");
+$reports['orpahan_tracks'] =  array("title"=>"Orpahn Tracks","sql"=>"select tracks.id,tracks.name from tracks left join albums on album_id=albums.id where albums.name is null");
 
 if(isset($_POST['report'])) {
 	$report = $_POST['report'];
