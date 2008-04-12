c3212e53b77c491f96cd56eaf412eedc8a972923
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-04-12 12:09:38 +0000

    feature: new tracks on homepage

--- index.php
+++ index.php
@@ -2,65 +2,14 @@
 <?php require("include/mpd.php"); ?>
 <?php include("header.inc.php"); ?>
 <h2>Latest Albums</h2>
-<ul>
+<div id="album_list">
 <?php
 $album = new album();
 $album->getNew(5);
-while($album->getNext()) { ?>
-	<li><a href="<?= album_link($album->id,$album->name) ?>"><?= $album->name ?></li>
-	<?php
+while($album->getNext()) { 
+	$album->displayThumb();
 } 
 ?>
-</ul>
-<?php
-if($data = mpd_now_playing()) {
-	?>
-	<h2>Currently Playing In Store</h2>
-	<table width="100%">
-	<tr>
-		<th>Artist :</th>
-		<td><?= $data['artist'] ?>
-			<?php 
-			$artist = new artist();
-			if($artist->getByOther(array('name'=>$data['artist']))) {
-				?>
-				(<a href="browse.php?type=artist&amp;id=<?= $artist->id ?>">View Tracks</a>)
-				<?php
-			}
-			?>
-		</td>
-		<td rowspan="3" align="center">
-		<?php
-		$album = new album();
-		if($album->getByOther(array('name'=>$data['album']))) {
-			?>
-			<a href="album.php?album_id=<?= $album->id ?>">
-			<?php
-			if($album->image_id) {
-				$image = new image();
-				$image->show($album->image_id,150,150);
-				print "<br/>";
-			}
-			?>
-			View Album</a>
-
-			<?php
-		}
-		?>
-		</td>
-	</tr>
-	<tr>
-		<th>Album :</th>
-		<td><?if($album->id) { ?><a href="album.php?album_id=<?= $album->id ?>"><? } ?><?= $data['album'] ?><? if($album->id) { ?></a><? } ?></td>
-	</tr>
-	<tr>
-		<th>Track :</th>
-		<td><?= $data['title'] ?> (<?= $data['time'] ?>)</td>
-	</tr>
-	</table>
-	<?php
-}
-
-?>
+</div>
 <?php include("footer.inc.php"); ?>
 
