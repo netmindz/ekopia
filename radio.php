a80384bd6b6b621cb80af25ae152f648758b3414
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-04-20 19:32:20 +0000

    feature: radio

---
+++ radio.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Ekopia Radio</title>
+</head>
+<body>
+<applet code="JOrbisPlayer.class"
+        codebase="http://www.jcraft.com/jorbis/player/"
+        archive="JOrbisPlayer-0.0.17.b5-rsa.jar"
+        width="600" height="35" align="baseline">
+        <PARAM NAME="jorbis.player.play.0" VALUE="http://ralf.netmindz.net:8000/ekopia.ogg">
+        <PARAM NAME="jorbis.player.icestats" VALUE="no">
+        <PARAM NAME="jorbis.player.playonstartup" VALUE="yes">
+</applet>
+</body>
+</html>
