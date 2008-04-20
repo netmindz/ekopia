e94c0815fce3f6a75bc6df3ba721874a8d68d84f
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-04-20 19:13:25 +0000

    feature: untagged albums report

--- admin/reports.php
+++ admin/reports.php
@@ -8,7 +8,7 @@ $reports['album_list'] =  array("title"=>"Album List","sql"=>"select id,name,pri
 $reports['unamed'] =  array("title"=>"Unnamed Tracks","sql"=>"select albums.id,albums.name,sum(if(tracks.name = '',1,0)) as unnamed from tracks left join albums on album_id=albums.id group by albums.id having unnamed > 0");
 $reports['orpahan_tracks'] =  array("title"=>"Orphan Tracks","sql"=>"select tracks.id,tracks.name from tracks left join albums on album_id=albums.id where albums.name is null");
 $reports['lonely_artists'] =  array("title"=>"Empty Artists","sql"=>"select artists.id,artists.name,count(tracks.artist_id) as track_count, count(albums.artist_id) as album_count from artists left join albums on artists.id=albums.artist_id left join tracks on artists.id=tracks.artist_id group by artists.id having album_count = 0 and track_count = 0");
-$reports['untagged_albums'] = array('title'=>'Untagged Albums','sql'=>'select id,albums.name,count(album_tags.id) as tag_count from albums left join album_tags on albums.id=album_FK order by albums.name group by albums.id having tag_count=0');
+$reports['untagged_albums'] = array('title'=>'Untagged Albums','sql'=>'select id,albums.name,count(album_tags.id) as tag_count from albums left join album_tags on albums.id=album_FK group by albums.id order by albums.name having tag_count=0');
 
 if(isset($_POST['report'])) {
 	$report = $_POST['report'];
