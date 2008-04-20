a6f37c6ccb65e0fa5252e03ec4c8eaedb29bee52
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-04-20 19:58:09 +0000

    feature: latest stuff

--- header.inc.php
+++ header.inc.php
@@ -76,6 +76,7 @@ href="http://www.inspiralled.net/templates/sp_main3/css/ie.css" />
 							<h3>Browse</h3>
 							<table width="100%" border="0" cellpadding="0" cellspacing="0">
 								<tr align="left"><td><a href="<?= browse_link("album") ?>" class="mainlevel" >Albums</a></td></tr>
+								<tr align="left"><td><a href="<?= $CONF['url'] ?>/new.php" class="mainlevel" >New Items</a></td></tr>
 								<tr align="left"><td><a href="<?= browse_link("artist") ?>" class="mainlevel" >Artists</a></td></tr>
 								<tr align="left"><td><a href="<?= browse_link("label") ?>" class="mainlevel" >Labels</a></td></tr>
 								<tr align="left"><td><a href="<?= $CONF['url'] ?>/tags.php" class="mainlevel" >Styles</a></td></tr>
---
+++ new.php
@@ -0,0 +1,14 @@
+<?php include("include/common.php"); ?>
+<?php include("header.inc.php"); ?>
+<h2>Latest Albums</h2>
+<div id="album_list">
+<?php
+$album = new album();
+$album->getNew(30);
+while($album->getNext()) { 
+	$album->displayThumb();
+} 
+?>
+</div>
+<?php include("footer.inc.php"); ?>
+
