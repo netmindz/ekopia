55b30d8ac8c55344c5462edbdd69e9a2b33e2943
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-07-01 18:05:10 +0000

    feature: google tracking

--- footer.inc.php
+++ footer.inc.php
@@ -64,6 +64,11 @@
   
   
 </div>
-
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-4868734-1";
+urchinTracker();
+</script>
 </body>
 </html><!-- 1206456645 -->
