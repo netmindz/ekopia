ee498b3f7a0f2a8cc52ded4f21010d8921c6818a
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-07-14 19:14:51 +0000

    update: new radio

--- radio.php
+++ radio.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <applet code="jcraft.player.JOrbisPlayer.class"
-        archive="jorbis.jar"
+        archive="jorbis.jar,swing-layout-1.0.3.jar"
         width="384" height="144" align="baseline">
         <PARAM NAME="jorbis.player.playonstartup" VALUE="yes">
 </applet>
