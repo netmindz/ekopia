23a60fc8cf10795b920ed3373dfe8478360d9cce
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-11-21 09:25:10 +0000

    update: use full path for playlists

--- playlist.php
+++ playlist.php
@@ -7,7 +7,7 @@ $track->getTrackListings($_REQUEST['album_id']);
 <player showDisplay="yes" showPlaylist="yes" autoStart="yes">
 <?php
 while($track->getNext()) { ?>
-	<song path="preview.php?track_id=<?= $track->id ?>" title="<?= htmlspecialchars($track->name) ?>" />
+	<song path="<?= $CONF['media_url'] ?>/preview.php?track_id=<?= $track->id ?>" title="<?= htmlspecialchars($track->name) ?>" />
 <?php
 }
 ?>
