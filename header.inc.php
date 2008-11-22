455e7acf2d4a92556856f35bafa0950cc691bdb4
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2008-11-22 13:47:41 +0000

    update: change side nav to be clearer

--- header.inc.php
+++ header.inc.php
@@ -86,8 +86,8 @@ href="http://www.inspiralled.net/templates/sp_main3/css/ie.css" />
 							<h3>Music</h3>
 							<table width="100%" border="0" cellpadding="0" cellspacing="0">
 								<tr align="left"><td><a href="<?= browse_link("album") ?>" class="mainlevel" >Albums</a></td></tr>
-								<tr align="left"><td><a href="<?= $CONF['url'] ?>/new.php" class="mainlevel" >New Items</a></td></tr>
-								<tr align="left"><td><a href="<?= $CONF['url'] ?>/downloads.php" class="mainlevel" >Downloads</a></td></tr>
+								<tr align="left"><td><a href="<?= $CONF['url'] ?>/new.php" class="mainlevel" >New Albums</a></td></tr>
+								<tr align="left"><td><a href="<?= $CONF['url'] ?>/downloads.php" class="mainlevel" >New Downloads</a></td></tr>
 								<tr align="left"><td><a href="<?= browse_link("artist") ?>" class="mainlevel" >Artists</a></td></tr>
 								<tr align="left"><td><a href="<?= browse_link("label") ?>" class="mainlevel" >Labels</a></td></tr>
 								<tr align="left"><td><a href="<?= $CONF['url'] ?>/tags.php" class="mainlevel" >Styles</a></td></tr>
