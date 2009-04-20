b682400a7c9285cd4f5b5985f1fef6dea24753ce
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-04-20 19:37:32 +0000

    feature: add single track playback support

--- playlist.php
+++ playlist.php
@@ -2,13 +2,22 @@
 require("include/common.php");
 
 $track = new track();
-$track->getTrackListings($_REQUEST['album_id']);
-?>
-<player showDisplay="yes" showPlaylist="yes" autoStart="yes">
-<?php
-while($track->getNext()) { ?>
-	<song path="<?= $CONF['media_url'] ?>/preview.php?track_id=<?= $track->id ?>" title="<?= htmlspecialchars($track->name) ?>" />
-<?php
+if(isset($_REQUEST['track_id'])) {
+	$track->get($_REQUEST['track_id']);
+	?>
+	<player showDisplay="no" showPlaylist="no" autoStart="yes">
+		<song path="<?= $CONF['media_url'] ?>/preview.php?track_id=<?= $track->id ?>" title="<?= htmlspecialchars($track->name) ?>" />
+	<?php
 }
-?>
+else {
+	$track->getTrackListings($_REQUEST['album_id']);
+	?>
+	<player showDisplay="yes" showPlaylist="yes" autoStart="yes">
+	<?php
+	while($track->getNext()) { ?>
+		<song path="<?= $CONF['media_url'] ?>/preview.php?track_id=<?= $track->id ?>" title="<?= htmlspecialchars($track->name) ?>" />
+	<?php
+	}
+	?>
+<? } ?>
 </player>
