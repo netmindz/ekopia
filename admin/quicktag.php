56dad21fd98d05310954ae9bac77872e5613386d
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-06-22 12:58:44 +0000

    feature: flexible skip value

--- admin/quicktag.php
+++ admin/quicktag.php
@@ -33,7 +33,7 @@ $track->getNext();
 <PARAM NAME="wmode" VALUE="transparent"> <param name="allowScriptAccess" value="sameDomain" /><param name="movie" value="<?= $CONF['media_url'] ?>/mp3player.swf?playlist=<?= $CONF['media_url'] ?>/playlist.php?track_id=<?= $track->id ?>" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" /><embed src="<?= $CONF['media_url'] ?>/mp3player.swf?playlist=<?= $CONF['media_url'] ?>/playlist.php?track_id=<?= $track->id ?>" quality="high" bgcolor="#ffffff" width="280" height="280" name="player" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" wmode="transparent" /></object>
 <form method="post">
 <input type="hidden" name="track_id" value="<?= $track->id ?>"/>
-<input type="text" name="offset" value="<?= $offset ?>" />
+<input type="hidden" name="offset" value="<?= $offset ?>" />
 <table>
 <tr>
 	<th><?= $track->DN ?></th>
@@ -45,7 +45,7 @@ $track->getNext();
 <input type="submit" value=" Update Track ">
 </form>
 <form  method="post" name="skip">
-<input type="text" name="offset" value="<?= ($offset+1) ?>" />
+<input type="hidden" name="offset" value="<?= ($offset+1) ?>" />
 Skip: <input type="text" size="2" name="skip" value="1" onChange="document.forms.skip.offset.value=document.forms.skip.skip.value+<?= $offset ?>"/> tracks 
 <input type="submit" value=" Skip ">
 </form>
