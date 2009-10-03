650b71e392a8171b17f3440863d74f1032f6c690
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-10-03 08:32:34 +0000

    update: add more text to radio player

--- radio.php
+++ radio.php
@@ -5,11 +5,18 @@
 <link href="http://www.inspiralled.net/css/content.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
+<h1>inSpiral Radio</h1>
 <applet code="jcraft.player.JOrbisPlayer.class"
         archive="jorbis.jar,swing-layout-1.0.3.jar"
         width="384" height="144" align="baseline">
         <PARAM NAME="jorbis.player.playonstartup" VALUE="yes">
 </applet>
-<p><a href="radio.jnlp">Install inSpiral Radio Player</a></p>
+<p>Want inSpiral Radio at your fingertips ? <a href="radio.jnlp">Install inSpiral Radio Player</a></p>
+<h2>Help</h2>
+<ul>
+	<li>You see no player above - you need to install <a href="http://www.java.com/">Java</a></li>
+	<li>You get some message about security - This is just to say that you wish to open the player, click trust and ok</li>
+	<li>Still having issues - Please email <a href="mailto:shop@inspiralled.net">shop@inpiralled.net<a></li>
+</ul>
 </body>
 </html>
