4f5371f1347034751d2a88f6ac00ed886e894ac7
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-10-03 09:14:21 +0000

    bugfix: use correct album link

--- radioinfo.php
+++ radioinfo.php
@@ -3,7 +3,7 @@ header("Content-type: text/plain");
 require("include/common.php"); 
 $album = new album();
 if($album->getByOther(array('name'=>$_REQUEST['album']))) {
-	print "album_link#http://shop.inspiralled.net" . browse_link("album",$album->id,$album->name)."\n";
+	print "album_link#http://shop.inspiralled.net" . album_link($album->id,$album->name)."\n";
 	if($album->image_id) {
 		print "image#http://shop.inspiralled.net/showimage.php?id=$album->image_id&width=120&height=120&nocache=true\n";
 	}
