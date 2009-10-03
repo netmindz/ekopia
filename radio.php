7be1c2e22c0cadd5efc3bb79b77b38fbcbcb9da5
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-10-03 09:34:20 +0000

    update: add version hack to player url

--- radio.php
+++ radio.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <applet code="jcraft.player.JOrbisPlayer.class"
-        archive="jorbis.jar,swing-layout-1.0.3.jar"
+        archive="jorbis.jar?2,swing-layout-1.0.3.jar"
         width="384" height="144" align="baseline">
         <PARAM NAME="jorbis.player.playonstartup" VALUE="yes">
 </applet>
