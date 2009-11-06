f474c7c01ff898f835287fa8162540403fa058d3
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-11-06 12:11:02 +0000

    feature: new layout for live

--- now3.php
+++ now3.php
@@ -25,10 +25,9 @@ $debug_style = "";
 <?php
 if($data = icecast_get_live()) {
 	?>
-<DIV  ID="TICKER" STYLE="<?= $debug_style ?> <?= $div_pos ?> overflow:hidden;">
+<DIV ID="TICKER" STYLE="<?= $debug_style ?> <?= $div_pos ?> overflow:hidden;"  onmouseover="TICKER_PAUSED=true" onmouseout="TICKER_PAUSED=false">
 <a href="http://www.inspiralled.net:8000/ekopia.ogg.m3u">Listen to live broadcast of <?= $data->artist ?> - <?= $data->title ?> direct from the lounge</a>
-
-<p>Requires <a href="http://download.nullsoft.com/winamp/client/winamp5541_full_emusic-7plus_en-us.exe">Winamp</a> or <a href="radio.php" onClick="window.open('radio.php','radio','menubar=0,resizable=1,width=400,height=200'); return false;" target="_new">inSpiral Radio Player</a> (requires java). Note: iTunes will not work, even if you have OggVorbis support
+.  Requires <a href="http://download.nullsoft.com/winamp/client/winamp5541_full_emusic-7plus_en-us.exe">Winamp</a> or <a href="radio.php" onClick="window.open('radio.php','radio','menubar=0,resizable=1,width=400,height=200'); return false;" target="_new">inSpiral Radio Player</a> (requires java). Note: iTunes will not work, even if you have OggVorbis support
 </div>
 	<?php
 }
