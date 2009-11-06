596df66ec9831f8312ca0e71706d1eb80f46fb8d
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-11-06 12:12:07 +0000

    feature: new layout for live

--- now3.php
+++ now3.php
@@ -29,6 +29,9 @@ if($data = icecast_get_live()) {
 <a href="http://www.inspiralled.net:8000/ekopia.ogg.m3u">Listen to live broadcast of <?= $data->artist ?> - <?= $data->title ?> direct from the lounge</a>
 .  Requires <a href="http://download.nullsoft.com/winamp/client/winamp5541_full_emusic-7plus_en-us.exe">Winamp</a> or <a href="radio.php" onClick="window.open('radio.php','radio','menubar=0,resizable=1,width=400,height=200'); return false;" target="_new">inSpiral Radio Player</a> (requires java). Note: iTunes will not work, even if you have OggVorbis support
 </div>
+<div style="position: absolute; left: 885px;">
+<h1>Live</h1>
+</div>
 	<?php
 }
 elseif($data = mpd_now_playing()) {
