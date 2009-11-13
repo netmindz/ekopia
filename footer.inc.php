c02bad9a57cf119439bf59592d964432e4b1ccdb
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-11-13 18:02:33 +0000

    update: new template

--- footer.inc.php
+++ footer.inc.php
@@ -1,72 +1,3 @@
-
-                                      </div>
-
-                                      <div class="module-bm">
-                                        <div class="module-bl"></div>
-
-                                        <div class="module-br"></div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div class="clr"></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div><!--End Main Column (col1wrap)-->
-                      <!--Begin Left Column (col2)-->
-                      <div class="col2">
-                        <div id="leftcol">
-                          <div class="sidecol-tm">
-                            <div class="sidecol-tl"></div>
-                            <div class="sidecol-tr"></div>
-                          </div>
-
-                          <div class="sidecol-m">
-                            <div class="sidecol-l">
-                              <div class="sidecol-r">
-                                <div class="">
-                                  <div class="moduletable">
-									<!-- side nav here -->
-                                    <div class="side-style-h3">
-										<h3 class="module-title">Browse</h3>
-                                    </div>
-                                    foo
-									<table width="100%" border="0" cellpadding="0" cellspacing="0">
-										<tr align="left"><td><a href="<?= browse_link("album") ?>" class="mainlevel" >Albums</a></td></tr>
-										<tr align="left"><td><a href="<?= browse_link("artist") ?>" class="mainlevel" >Artists</a></td></tr>
-										<tr align="left"><td><a href="<?= browse_link("label") ?>" class="mainlevel" >Labels</a></td></tr>
-										<!-- <tr align="left"><td><a href="browse.php?type=type" class="mainlevel" >Genres</a></td></tr> -->
-									</table>		
-
-<!--
-                                    <div class="side-style-h3">
-										<h3 class="module-title">Search</h3>
-                                    </div>
-									<form action="<?= $CONF['url'] ?>/search.php" method="post">
-									<input type="text" name="keyword" class="inputbox" size="10"/>
-									</form>
--->
-		
-									<div class="side-style-h3">
-										<h3 class="module-title">Products</h3>
-									</div>
-									foo
-									<table width="100%" border="0" cellpadding="0" cellspacing="0">
-									<?php
-									$nav_type = new type();
-									$nav_type->getList("where type_id=0");
-									while($nav_type->getNext()) {
-											?>
-											<tr align="left"><td><a href="<?= $CONF['url'] ?>/type.php?id=<?= $nav_type->id ?>" class="mainlevel" ><?= $nav_type->DN ?>s</a></td></tr>
-									<?php } ?>
-									</table>
-
-                                    <div class="module-inner">
-                                    </div>
-                                  </div>
                                 </div>
                               </div>
                             </div>
@@ -74,121 +5,21 @@
 
                           <div class="sidecol-bm">
                             <div class="sidecol-bl"></div>
-
                             <div class="sidecol-br"></div>
                           </div>
+
                         </div>
                       </div>
-                      <!--End Left Column (col2)-->
-                      <!--Begin Right Column (col3)-->
-					<?php
-					/*
-                      <div class="col3">
-                        <div id="rightcol">
-                          <div class="sidecol-tm">
-                            <div class="sidecol-tl"></div>
-
-                            <div class="sidecol-tr"></div>
-                          </div>
-
-                          <div class="sidecol-m">
-                            <div class="sidecol-l">
-                              <div class="sidecol-r">
-                                <div class="">
-                                  <div class="moduletable">
-                                    <div class="module-inner">
-                                      <p style="text-align: center;"><a href="http://sandbox.ralf.netmindz.net/index.php?option=com_content&amp;view=article&amp;id=239&amp;Itemid=330"><img src="http://sandbox.ralf.netmindz.net/images/stories/events/Flyers/2009/General/christmas.jpg" border="0" /></a></p>
-
-                                      <p style="text-align: center;"></p>
-
-                                      <p style="text-align: left;"></p>
-                                    </div>
-                                  </div>
-                                </div>
-
-                                <div class="">
-                                  <div class="moduletable">
-                                    <div class="module-inner">
-                                      <p style="text-align: center;">Superfoods now available in our online Shop!</p>
-
-                                      <p style="text-align: center;"><a href="http://sandbox.ralf.netmindz.net/index.php?option=com_content&amp;view=article&amp;id=140&amp;Itemid=265"><img src="http://sandbox.ralf.netmindz.net/images/stories/Products/Superfood_Labels/detox-mix-large.jpg" border="0" width="190" /></a></p>
-
-                                      <p style="text-align: left;"></p>
-                                    </div>
-                                  </div>
-                                </div>
-
-                                <div class="">
-                                  <div class="moduletable">
-                                    <div class="side-style-h3">
-                                      <h3 class="module-title">Lounge Hours</h3>
-                                    </div>
-
-                                    <div class="module-inner">
-                                      <ul>
-                                        <li>Monday to Friday</li>
-
-                                        <li>10:00 - 22:00</li>
-
-                                        <li>Saturday to Sunday</li>
-
-                                        <li>10:00 - 2:00 (late)</li>
-
-                                        <li>Sunday</li>
-
-                                        <li>10:00 - 23:30</li>
-                                      </ul>
-
-                                      <p style="text-align: left;"></p>
-                                    </div>
-                                  </div>
-                                </div>
-
-                                <div class="">
-                                  <div class="moduletable">
-                                    <div class="side-style-h3">
-                                      <h3 class="module-title">Meet the Team</h3>
-                                    </div>
-
-                                    <div class="module-inner">
-                                      <table width="100%" class="minifp">
-                                        <tr>
-                                          <td valign="top" width="100%" class="minifp">
-                                            <br class="minifp-seperator" />
-                                            <a href="http://sandbox.ralf.netmindz.net/index.php?option=com_content&amp;view=article&amp;id=202%3Alounge&amp;catid=68%3Aintro-meet-team&amp;Itemid=238"><img src="http://sandbox.ralf.netmindz.net/images/stories/minifp//kitchen-team_thumb.jpg" width="64" height="64" align="left" alt="article thumbnail" /></a>
-
-                                            <p>who are the people behind the unique inSpiral concept? We are sure you must be curious to find out who all the amazing peeps behind the inSpiral curtains are. Well, it sure is busy there with now m&nbsp;[&nbsp;...&nbsp;]</p><br class="minifp-seperator" />
-
-                                            <div class="minifp-full-link-wrp">
-                                              <a class="minifp-full-link" href="http://sandbox.ralf.netmindz.net/index.php?option=com_content&amp;view=article&amp;id=202%3Alounge&amp;catid=68%3Aintro-meet-team&amp;Itemid=238">+ Full Story</a>
-                                            </div>
-                                          </td>
-                                        </tr>
-                                      </table>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-
-                          <div class="sidecol-bm">
-                            <div class="sidecol-bl"></div>
-
-                            <div class="sidecol-br"></div>
-                          </div>
-                        </div>
-                      </div>
-                      */
-                      ?>
-                      <!--End Right Column (col3)-->
+					                      <!--End Right Column (col3)-->
                     </div>
                   </div>
                 </div>
               </div>
             </div><!--Begin Main Bottom-->
             <!--End Main Bottom-->
+
           </div>
+
         </div>
       </div>
     </div>
@@ -197,6 +28,7 @@
       <div class="main-bl"></div>
 
       <div class="main-br"></div>
+
     </div><!--End Main Body-->
   </div><!--Begin Footer-->
 
@@ -206,14 +38,18 @@
         <div id="mainmodules4" class="spacer w33">
           <div class="block first">
             <div class="">
+
               <div class="moduletable">
                 <div class="module-padding">
+
                   <h3 class="showcase">inSpiral Shop</h3>
 					&copy; <a href="http://www.netmindz.net/?shop">WJT 2009</a> || <a href="froogle.php?download">RSS</a>
 
                   <!-- content bits here -->
+
                 </div>
               </div>
+
             </div>
           </div>
 
@@ -221,14 +57,13 @@
             <div class="">
               <div class="moduletable">
                 <div class="module-padding">
+
                   <h3 class="showcase">Contact Us</h3>
 
                   <p>250 Camden High Street<br />
                   London<br />
                   NW1 8QS</p>
-
                   <p>office: 0207 419 6798</p>
-
                   <p>lounge: 0207 4825875</p>
 
                   <p><a href="mailto:dom@inspiralled.net">e-mail</a></p>
@@ -239,12 +74,14 @@
 
           <div class="block last">
             <div class="">
+
               <div class="moduletable">
                 <div class="module-padding">
                   <h3 class="showcase">Newsletter</h3>
 
                   <p>We are working hard on our new newsletter '<strong>inSpiral eyes</strong>' - full of exciting information about our events, food and community interests. To subscribe just sign up <a href="http://www.idspiral.org/cgi-local/dada/mail.cgi/list/inspiralled/">here</a>.</p>
                 </div>
+
               </div>
             </div>
           </div>
@@ -254,7 +91,9 @@
           <div class="footer-div"></div>
 
           <div id="top-button">
+
             <a href="#" id="top-scroll" class="top-button-desc" name="top-scroll">Back to Top</a>
+
           </div>
         </div><!--End Copyright Section-->
       </div>
@@ -263,6 +102,7 @@
 
       <div id="footer-bg3"></div>
     </div>
+
   </div><!--End Footer-->
 
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
--- header.inc.php
+++ header.inc.php
@@ -31,36 +31,46 @@ $template_root = "http://sandbox.ralf.netmindz.net";
   <link rel="stylesheet" href="<?= $template_root ?>/templates/rt_infuse_j15/css/menu-fusion.css" type="text/css" />
   <style type="text/css">
     <!--
-
 	div.wrapper { margin: 0 auto; width: 982px;padding:0;}
 	body { min-width:982px;}
 	#inset-block-left { width:0px;padding:0;}
 	#inset-block-right { width:0px;padding:0;}
 	#maincontent-block { margin-right:0px;margin-left:0px;}
-	
-	/* WJT */
+	.moduletable1 { background-color:#eae9b8;}
+	.side-style-h3 { background-color:#f1eed9;}
+	.sidecol-m { background-color:#eae9b8;}
+	.topic1 {padding:10px;}
+	#album_list {padding:10px;}
+	.mainlevel {text-decoration: none;text-indent: 0pt;color: #225769;font-weight: normal;font-style: normal;font-size: 110%;font-family:Arial;}
+	.mainlevel:link, .mainlevel:visited, .mainlevel:active { font-style: normal;color: #225769;text-decoration: none;text-indent: 0pt;font-weight: normal;}
+	.mainlevel:hover {color: #9fb400;font-style: normal;text-decoration: none;text-indent: 0pt;font-weight:lighter; }
+	.ccc{text-decoration: none;text-indent: 0pt;color: #225769;font-weight: normal;font-style: normal;font-size: 110%;font-family:Arial; }
+
+
+
+
 	.s-c-s .colmid { left:180px; }
 	.s-c-s .colright { margin-left:-510px; } 
-	.s-c-s .col1pad { margin-left:550px; } 
-	.s-c-s .col2 { left:330px; width:160px;}
-/*	.s-c-s .col3 { width:230px;} */
-	.s-c-s .col3 { width:0px;}
-	
+	.s-c-s .col1pad { margin-left:600px; } 
+	.s-c-s .col2 { left:330px; width:280px;}
+
+	.s-c-s .col3 {  margin-left:-380px; width:700px;}
+
 	.s-c-x .colright { left:280px;}
 	.s-c-x .col1wrap { right:280px;}
 	.s-c-x .col1 { margin-left:280px; }
 	.s-c-x .col2 { right:280px;width:280px;}
-	
+
 	.s-c-s .col1pad {
-		overflow-x: auto;
-		overflow-y: auto;
+	overflow-x: auto;
+	overflow-y: auto;
 	}
-	/* WJT End */
-	
+
+
 	.x-c-s .colright { margin-left:-230px; }
 	.x-c-s .col1 { margin-left:230px; border: 1px green solid;}
 	.x-c-s .col3 { left:230px;width:230px;}
-	
+
     -->
   </style>
 
@@ -105,21 +115,22 @@ InputsExclusion.push('.content_vote')
 </head>
 	
 <body id="ff-infuse" class="f-infuse style1 full iehandle">
+
   <!--Begin Header-->
 
   <div id="header">
+
     <div class="wrapper">
       <div class="padding">
         <!--Begin Logo-->
         <!--End Logo-->
         <div class="myBox3">
           <!--start radio-->
-
           <div>
-            <iframe name="basefrm" src="http://shop.inspiralled.net/now3.php" width="975" height="230" frameborder="0" scrolling="no" marginwidth="0" marginheight="0" align="left" allowtransparency="true" id="basefrm"></iframe>
+            <iframe name="basefrm" src="<?= $CONF['url'] ?>/now3.php" width="975" height="230" frameborder="0" scrolling="no" marginwidth="0" marginheight="0" align="left" allowtransparency="true" id="basefrm"></iframe>
+
           </div><!--end radio-->
         </div>
-
       </div>
     </div>
   </div><!--End Header-->
@@ -128,19 +139,23 @@ InputsExclusion.push('.content_vote')
     <!--Begin Showcase-->
 
     <div class="show-tm">
-      <div class="show-tl"></div>
 
+      <div class="show-tl"></div>
       <div class="show-tr"></div>
     </div>
 
     <div class="show-m">
       <div class="show-l">
         <div class="show-r">
+
+
 			<?php include("menu.inc.php"); ?>	
+
           <!--Begin Showcase Modules-->
           <!--End Showcase Modules-->
         </div>
       </div>
+
     </div>
 
     <div class="show-bm">
@@ -154,7 +169,6 @@ InputsExclusion.push('.content_vote')
 
     <div class="main-tm">
       <div class="main-tl"></div>
-
       <div class="main-tr"></div>
     </div>
 
@@ -162,6 +176,7 @@ InputsExclusion.push('.content_vote')
       <div class="main-l">
         <div class="main-r">
           <div id="main-body">
+
             <div id="main-content" class="s-c-s">
               <div class="colmask leftmenu">
                 <div class="wrapper">
@@ -171,22 +186,110 @@ InputsExclusion.push('.content_vote')
 
                       <div class="col1wrap">
                         <div class="col1pad">
+
                           <div class="col1">
                             <div id="maincol">
                               <div class="bodycontent">
                                 <div id="maincontent-block">
                                   <div class="">
                                     <div id="page" class="full-article">
-                                      <div class="module-tm">
-                                        <div class="module-tl"></div>
-
-                                        <div class="module-tr"></div>
-                                      </div>
-
                                       <div class="module-inner">
+                                      </div>                                    
+                                    </div>
 
-		<div class="article-rel-wrapper">
-	    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="clr"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div><!--End Main Column (col1wrap)-->
+                      <!--Begin Left Column (col2)-->
+
+                      <div class="col2">
+                        <div id="leftcol">
+
+                          <div class="sidecol-tm">
+                            <div class="sidecol-tl"></div>
+                            <div class="sidecol-tr"></div>
+                          </div>
+
+                          <div class="sidecol-m">
+                            <div class="sidecol-l">
+
+                              <div class="sidecol-r">
+                                <div class="moduletable1">
+
+                                  <div class="moduletable">
+									<!-- side nav here -->
+                                    <div class="side-style-h3">
+										<h3 class="module-title">Browse</h3>
+									</div>
+									<table width="100%" border="0" cellpadding="0" cellspacing="0">
+
+										<tr><td>&nbsp;</td></tr>
+										<tr align="left" cellpadding="5" cellspacing="5"><td>&nbsp;&nbsp;<img src="<?= $template_root ?>/templates/rt_infuse_j15/images/style1/menu-button-shop.png">&nbsp;&nbsp;<a href="<?= browse_link("album") ?>">Albums</a></td></tr>
+										<tr><td><img src="<?= $template_root ?>/templates/rt_infuse_j15/images/style1/line.png"></td></tr>
+										<tr><td>&nbsp;</td></tr>
+										<tr align="left" cellpadding="5" cellspacing="5"><td>&nbsp;&nbsp;<img src="<?= $template_root ?>/templates/rt_infuse_j15/images/style1/menu-button-shop.png">&nbsp;&nbsp;<a href="<?= browse_link("artist") ?>">Artists</a></td></tr>
+										<tr><td><img src="<?= $template_root ?>/templates/rt_infuse_j15/images/style1/line.png"></td></tr>
+										<tr><td>&nbsp;</td></tr>
+										<tr align="left" cellpadding="5" cellspacing="5"><td>&nbsp;&nbsp;<img src="<?= $template_root ?>/templates/rt_infuse_j15/images/style1/menu-button-shop.png">&nbsp;&nbsp;<a href="<?= browse_link("label") ?>">Labels</a></td></tr>
+
+										<tr><td><img src="<?= $template_root ?>/templates/rt_infuse_j15/images/style1/line.png"></td></tr>
+									</table>
+									
+		
+									<div class="side-style-h3">
+										<h3 class="module-title">Products</h3>
+									</div>
+									<table width="100%" border="0" cellpadding="0" cellspacing="0">
+									<?php
+									$nav_type = new type();
+									$nav_type->getList("where type_id=0");
+									while($nav_type->getNext()) {
+											?>
+											<tr><td>&nbsp;</td></tr>
+											<tr align="left" cellpadding="5" cellspacing="5"><td>&nbsp;&nbsp;<img src="<?= $template_root ?>/templates/rt_infuse_j15/images/style1/menu-button-shop.png">&nbsp;&nbsp;<a href="<?= $CONF['url'] ?>/type.php?id=<?= $nav_type->id ?>"><?= $nav_type->DN ?>s</a></td></tr>
+									<?php } ?>
+									</table>
+								
+
+                                    <div class="module-inner">
+                                    </div>
+
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="sidecol-bm">
+                            <div class="sidecol-bl"></div>
+                            <div class="sidecol-br"></div>
+
+                          </div>
+                        </div>
+                      </div>
+                      <!--End Left Column (col2)-->
+
+                      <!--Begin Right Column (col3)-->
+					  <div class="col3">
+                        <div id="leftcol">
+
+                          <div class="sidecol-tm">
+
+                            <div class="sidecol-tl"></div>
+                            <div class="sidecol-tr"></div>
+                          </div>
+
+                          <div class="sidecol-m">
+                            <div class="sidecol-l">
+                              <div class="sidecol-r" >
+                                <div class="moduletable1">
+                                  <div class="moduletable">
+
 
 <!-- real start of content -->
 <?php
