96badfe18d597656b620ae25d68005f2259130eb
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-11-30 15:48:57 +0000

    feature: latest page

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php include("include/common.php"); ?>
 <?php require("include/mpd.php"); ?>
 <?php include("header.inc.php"); ?>
-<h2>Latest Albums</h2>
-<div id="album_list">
-<?php
-$album = new album();
-$album->getNew(5);
-while($album->getNext()) { 
-	$album->displayThumb();
-} 
-?>
-</div>
-<?php include("footer.inc.php"); ?>
 
+<?php include("footer.inc.php"); ?>
