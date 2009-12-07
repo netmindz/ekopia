9fc718d4099a7954f8182f6b6db46aa712bcf611
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-12-07 20:32:10 +0000

    feature: cache icecast status

--- include/icecast.php
+++ include/icecast.php
@@ -1,7 +1,17 @@
 <?php
 function icecast_get_live()
 {
-	$xml = @file_get_contents("http://master.inspiralled.net:8000/info.xsl");
+	$url = "http://master.inspiralled.net:8000/info.xsl";
+	$cache = "/tmp/icecast-" . md5($url);
+	if(!is_file($cache)||((time() - filemtime($cache) > 30))) {
+		$xml = @file_get_contents($url);
+		if($xml) {
+			file_put_contents($cache,$xml);
+		}
+	}
+	else {
+		$xml = file_get_contents($cache);
+	}
 	if($xml) {
 		$doc = simplexml_load_string($xml);
 		foreach($doc->source as $source) {
