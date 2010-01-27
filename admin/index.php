4358ad770b649218271b63355d169db90c3547f8
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2010-01-27 22:34:27 +0000

    bugfix: missing class includes

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 require("../include/site_config.inc.php");
+require("user_artist.class.php");
+require("user_label.class.php");
 $is_admin_area = true;
 require("/home/www/codebase/premier_generic.php");
 ?>
