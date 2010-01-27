4a4020bc292317a0491cea368bd773a4eee6ac71
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2010-01-27 22:32:50 +0000

    bugfix: non-public tags in admin area

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 require("../include/site_config.inc.php");
-require("/home/www/codebase/premier_generic.php");
 $is_admin_area = true;
+require("/home/www/codebase/premier_generic.php");
 ?>
